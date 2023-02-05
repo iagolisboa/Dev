91163876d61977780915f2938208a94641cfa651
Iago Lisboa <iagolisboa@hotmail.com>
2023-02-05 20:13:52 -0300

    Testing stubs to intercept requests

--- Alura/First Cypress project/cypress/e2e/3-ALURAPIC/login.cy.js
+++ Alura/First Cypress project/cypress/e2e/3-ALURAPIC/login.cy.js
@@ -3,9 +3,13 @@ describe("Login users on alura pic", ()=> {
     const users = require('../../fixtures/users.json');
     beforeEach(() =>{
         cy.visit("https://alura-fotos.herokuapp.com")
+        cy.intercept("POST", "https://apialurapic.herokuapp.com/user/login", {
+            statusCode: 400
+        }).as("stubPost")
     })
         it("Login with valid user", () =>{
             cy.login(Cypress.env("userName"), Cypress.env('password'))
+            cy.wait('@stubPost')
             cy.contains('a','(Logout').should('be.visible')
         })
         it("Login with invalid user", () =>{
